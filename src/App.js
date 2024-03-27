60f8ed06e79e3c1be2a321018d4171ad6148309d
HaiderSana338 <141793214+HaiderSana338@users.noreply.github.com>
2024-03-28 10:24:34 +1100

    New Update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Container } from "react-bootstrap";
-import logo from "./logo.svg";
-import './App.css';
 
-import HomePage from './routes/HomePage';
-import GridViewPage from './routes/GridViewPage';
-import DetailsPage from './routes/DetailsPage';
+import "./App.css";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import HomePage from "./pages/HomePage";
+import GridViewPage from "./pages/GridViewPage";
+import
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Hello All and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+    <Container fluid>
+    <Header />
+    <Routes>
+    <Route path="/" element={<HomePage/>} />
+    <Route path="/gridview" element={<GridViewPage />} />
+    <Route path="/listitem/:id" element={<ListItemDetailsPage />} />
+    <Route path="*"element={<NotFoundPage />} />
+    </Routes>
+    <Footer />
+    </Container>
+    </Router>
+    );
+    
 }
 
 export default App;
